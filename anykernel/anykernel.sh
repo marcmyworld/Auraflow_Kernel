30396745e6e8deef6d07e11cd1ee522880860c2c
marcmyworld <moviesminister@gmail.com>
2026-09-21 16:50:58 +0530

    auraflow: Initialize Auraflow-Balanced on Linux 6.1.174 LTS

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -2,334 +2,75 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# begin properties
+# global properties
 properties() { '
-kernel.string=RealKing Kernel by Rohail(@Rohail33)--Telegram
-do.devicecheck=0
+kernel.string=Auraflow Kernel for SM8635 / cliffs (chenfeng & peridot)
+do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name3=
+device.name1=chenfeng
+device.name2=chenfengin
+device.name3=peridot
 device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-### AnyKernel install
 
+### AnyKernel install
 ## boot shell variables
 block=boot
-is_slot_device=1
+is_slot_device=auto
 ramdisk_compression=auto
 patch_vbmeta_flag=auto
-vendor_dlkm_partition=vendor_dlkm
+no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
 
+kernel_version=$(cat /proc/version | awk '{print $3}')
+
+# Dynamic device identification
+device_codename=$(getprop ro.product.device 2>/dev/null)
+[ -z "$device_codename" ] && device_codename=$(getprop ro.build.product 2>/dev/null)
+[ -z "$device_codename" ] && device_codename=$(getprop ro.product.vendor.device 2>/dev/null)
+[ -z "$device_codename" ] && device_codename=$(getprop ro.vendor.product.device 2>/dev/null)
+
+model_name=$(getprop ro.product.model 2>/dev/null)
+[ -z "$model_name" ] && model_name=$(getprop ro.product.vendor.model 2>/dev/null)
+
+case "$device_codename" in
+  chenfeng|chenfengin)
+    display_device="Xiaomi 14 Civi / Civi 4 Pro"
+    ;;
+  peridot)
+    display_device="POCO F6 / Redmi Turbo 3"
+    ;;
+  *)
+    if [ -n "$model_name" ]; then
+      display_device="$model_name"
+    else
+      display_device="SM8635 (cliffs)"
+    fi
+    ;;
+esac
+
+ui_print " "
+ui_print "========================================"
+ui_print "            AURAFLOW KERNEL             "
+ui_print "        SM8635 / cliffs Unified         "
+ui_print "========================================"
+ui_print " "
+ui_print "- Detected Device:  $display_device ($device_codename)"
+ui_print "- Current Kernel:   $kernel_version"
+ui_print "- Target Partition: /dev/block/by-name/boot"
+ui_print "- Installing Auraflow Kernel Image..."
+
+# boot install (Android 14 GKI device with init_boot partition)
 split_boot
-
-########## CUSTOM START ##########
-
-BOOTMODE=false;
-ps | grep zygote | grep -v grep >/dev/null && BOOTMODE=true;
-$BOOTMODE || ps -A 2>/dev/null | grep zygote | grep -v grep >/dev/null && BOOTMODE=true;
-
-
-extract_erofs() {
-	local img_file=$1
-	local out_dir=$2
-
-	${bin}/extract.erofs -i $img_file -x -T8 -o $out_dir &> /dev/null
-}
-
-mkfs_erofs() {
-	local work_dir=$1
-	local out_file=$2
-
-	local partition_name=$(basename $work_dir)
-
-	${bin}/mkfs.erofs \
-		--mount-point /${partition_name} \
-		--fs-config-file ${work_dir}/../config/${partition_name}_fs_config \
-		--file-contexts  ${work_dir}/../config/${partition_name}_file_contexts \
-		-z lz4hc \
-		$out_file $work_dir
-}
-
-is_mounted() { mount | grep -q " $1 "; }
-
-resolve_modules_dir() {
-	local extract_dir=$1
-	local partition_name=$2
-	local modules_dir
-
-	for modules_dir in \
-		"${extract_dir}/lib/modules" \
-		"${extract_dir}/${partition_name}/lib/modules"; do
-		[ -d "$modules_dir" ] && {
-			echo "$modules_dir"
-			return 0
-		}
-	done
-
-	modules_dir=$(find "$extract_dir" -type d -path "*/lib/modules" | grep -v "/config/" | head -n1)
-	[ -n "$modules_dir" ] || return 1
-	echo "$modules_dir"
-	return 0
-}
-
-resolve_all_modules_dirs() {
-	local extract_dir=$1
-	local partition_name=$2
-	local modules_dir all_dirs
-
-	all_dirs=""
-	for modules_dir in \
-		"${extract_dir}/lib/modules" \
-		"${extract_dir}/${partition_name}/lib/modules"; do
-		[ -d "$modules_dir" ] || continue
-		case " $all_dirs " in
-			*" ${modules_dir} "*) ;;
-			*) all_dirs="${all_dirs} ${modules_dir}" ;;
-		esac
-	done
-
-	for modules_dir in $(find "$extract_dir" -type d -path "*/lib/modules" | grep -v "/config/"); do
-		case " $all_dirs " in
-			*" ${modules_dir} "*) ;;
-			*) all_dirs="${all_dirs} ${modules_dir}" ;;
-		esac
-	done
-
-	[ -n "$all_dirs" ] || return 1
-	echo "$all_dirs"
-	return 0
-}
-
-find_named_block() {
-	local partition_name=$1
-	local partition_path
-
-	for partition_path in /dev/block/mapper /dev/block/by-name /dev/block/bootdevice/by-name; do
-		[ -e ${partition_path}/${partition_name}${slot} ] && {
-			echo ${partition_path}/${partition_name}${slot}
-			return 0
-		}
-		[ -e ${partition_path}/${partition_name} ] && {
-			echo ${partition_path}/${partition_name}
-			return 0
-		}
-	done
-
-	return 1
-}
-
-detect_ramdisk_compression() {
-	local file_path=$1
-	local magic
-
-	magic=$(dd if="$file_path" bs=6 count=1 2>/dev/null | od -An -tx1 | tr -d ' \n')
-	case "$magic" in
-		1f8b08*) echo gzip ;;
-		fd377a585a00*) echo xz ;;
-		425a68*) echo bzip2 ;;
-		5d0000*) echo lzma ;;
-		04224d18*) echo lz4 ;;
-		02214c18*) echo lz4_legacy ;;
-		*) echo none ;;
-	esac
-}
-
-# Check snapshot status
-# Technical details: https://blog.xzr.moe/archives/30/
-${bin}/snapshotupdater_static dump &>/dev/null
-rc=$?
-if [ "$rc" != 0 ]; then
-	ui_print "Cannot get snapshot status via snapshotupdater_static! rc=$rc."
-	if $BOOTMODE; then
-		ui_print "If you are installing the kernel in an app, try using another app."
-		ui_print "Recommend KernelFlasher:"
-		ui_print "  https://github.com/capntrips/KernelFlasher/releases"
-	else
-		ui_print "Please try to reboot to system once before installing!"
-	fi
-	abort "Aborting..."
-fi
-snapshot_status=$(${bin}/snapshotupdater_static dump 2>/dev/null | grep '^Update state:' | awk '{print $3}')
-ui_print "Current snapshot state: $snapshot_status"
-if [ "$snapshot_status" != "none" ]; then
-	ui_print " "
-	ui_print "Seems like you just installed a rom update."
-	if [ "$snapshot_status" == "merging" ]; then
-		ui_print "Please use the rom for a while to wait for"
-		ui_print "the system to complete the snapshot merge."
-		ui_print "It's also possible to use the \"Merge Snapshots\" feature"
-		ui_print "in TWRP's Advanced menu to instantly merge snapshots."
-	else
-		ui_print "Please try to reboot to system once before installing!"
-	fi
-	abort "Aborting..."
-fi
-unset rc snapshot_status
-
-vendor_dlkm_block=$(find_named_block ${vendor_dlkm_partition})
-[ -n "$vendor_dlkm_block" ] || abort "! Failed to find ${vendor_dlkm_partition} partition"
-
-# Check vendor_dlkm partition status
-[ -d /vendor_dlkm ] || mkdir /vendor_dlkm
-is_mounted /vendor_dlkm || \
-	mount /vendor_dlkm -o ro || mount ${vendor_dlkm_block} /vendor_dlkm -o ro || \
-		abort "! Failed to mount /vendor_dlkm"
-
-strings ${home}/Image 2>/dev/null | grep -E -m1 'Linux version.*#' > ${home}/vertmp
-
-skip_update_flag=false
-do_backup_flag=false
-if [ -f /vendor_dlkm/lib/modules/vertmp ]; then
-	[ "$(cat /vendor_dlkm/lib/modules/vertmp)" == "$(cat ${home}/vertmp)" ] && skip_update_flag=true
-else
-	do_backup_flag=true
-fi
-umount /vendor_dlkm
-
-# Fix unable to mount image as read-write in recovery
-$BOOTMODE || setenforce 0
-
-#if $skip_update_flag; then
-#else
-	# Dump vendor_dlkm partition image
-	dd if=${vendor_dlkm_block} of=${home}/vendor_dlkm.img
-
-	# Backup kernel and vendor_dlkm image
-	#if $do_backup_flag; then
-		ui_print "- It looks like you are installing Realking Kernel for the first time."
-		ui_print "- Next will backup the kernel and vendor_dlkm partitions..."
-
-		build_prop=/system/build.prop
-		[ -d /system_root/system ] && build_prop=/system_root/$build_prop
-		backup_package=/sdcard/Realking-restore-kernel-$(file_getprop $build_prop ro.build.version.incremental)-$(date +"%Y%m%d-%H%M%S").zip
-		${bin}/7za a -tzip -bd $backup_package \
-			${home}/META-INF ${bin} ${home}/LICENSE ${home}/_restore_anykernel.sh ${split_img}/kernel ${home}/vendor_dlkm.img
-		${bin}/7za rn -bd $backup_package Image.gz
-		${bin}/7za rn -bd $backup_package _restore_anykernel.sh anykernel.sh
-		sync
-
-		ui_print " "
-		ui_print "- The current kernel and gevendor_dlkm have been backedup to:"
-		ui_print "  $backup_package"
-		ui_print "- If you encounter an unexpected situation,"
-		ui_print "  or want to restore the stock kernel,"
-		ui_print "  please flash it in TWRP or some supported apps."
-		ui_print " "
-		touch ${home}/do_backup_flag
-
-		unset build_prop backup_package
-	#fi
-
-	ui_print "- Unpacking /vendor_dlkm partition..."
-	extract_vendor_dlkm_dir=${home}/_extract_vendor_dlkm
-	mkdir -p $extract_vendor_dlkm_dir
-	vendor_dlkm_is_ext4=false
-	vendor_dlkm_erofs_root_dir=""
-	extract_erofs ${home}/vendor_dlkm.img $extract_vendor_dlkm_dir || vendor_dlkm_is_ext4=true
-	sync
-
-	if $vendor_dlkm_is_ext4; then
-		ui_print "- /vendor_dlkm partition seems to be in ext4 file system."
-		mount ${home}/vendor_dlkm.img $extract_vendor_dlkm_dir -o ro -t ext4 || \
-			abort "! Unsupported file system!"
-		vendor_dlkm_free_space=$(df -k | grep -E "[[:space:]]$extract_vendor_dlkm_dir\$" | awk '{print $4}')
-		umount $extract_vendor_dlkm_dir
-
-		[ "$vendor_dlkm_free_space" -gt 10240 ] || {
-			# Resize vendor_dlkm image
-			ui_print "- /vendor_dlkm partition does not have enough free space!"
-			ui_print "- Trying to resize..."
-
-			${bin}/e2fsck -f -y ${home}/vendor_dlkm.img
-			vendor_dlkm_target_size_mb="128M"
-			${bin}/resize2fs ${home}/vendor_dlkm.img $vendor_dlkm_target_size_mb || \
-				abort "! Failed to resize vendor_dlkm image!"
-			ui_print "- Resized vendor_dlkm.img size: ${vendor_dlkm_target_size_mb}M."
-			# e2fsck again
-			${bin}/e2fsck -f -y ${home}/vendor_dlkm.img
-
-			unset super_free_space vendor_dlkm_current_size_mb vendor_dlkm_target_size_mb
-		}
-
-                {bin}/e2fsck -y -E unshare_blocks ${home}/vendor_dlkm.img
-                 ui_print "Removing unshared blocks from vendor_dlkm.img partition.."
-
-		ui_print "- Trying to mount vendor_dlkm image as read-write..."
-		mount ${home}/vendor_dlkm.img $extract_vendor_dlkm_dir -o rw -t ext4 || \
-			abort "! Failed to mount vendor_dlkm.img as read-write!"
-	fi
-	extract_vendor_dlkm_modules_dir=$(resolve_modules_dir "$extract_vendor_dlkm_dir" "$vendor_dlkm_partition") || \
-		abort "! Failed to locate ${vendor_dlkm_partition} modules directory"
-	if ! $vendor_dlkm_is_ext4; then
-		vendor_dlkm_erofs_root_dir=$(resolve_erofs_root_dir "$extract_vendor_dlkm_dir" "$vendor_dlkm_partition") || \
-			abort "! Failed to locate ${vendor_dlkm_partition} erofs root directory"
-	fi
-	search_vendor_dlkm_modules_dir=${extract_vendor_dlkm_modules_dir}
-
-	ui_print "- Updating /vendor_dlkm image..."
-	cp -f ${home}/_modules/*.ko ${extract_vendor_dlkm_modules_dir}/
-	cp -f ${home}/vertmp ${extract_vendor_dlkm_modules_dir}/vertmp
-	sync
-
-	if $vendor_dlkm_is_ext4; then
-		set_perm 0 0 0644 ${extract_vendor_dlkm_modules_dir}/vertmp
-		chcon u:object_r:vendor_file:s0 ${extract_vendor_dlkm_modules_dir}/vertmp
-		umount $extract_vendor_dlkm_dir
-	else
-		cat ${extract_vendor_dlkm_dir}/config/vendor_dlkm_fs_config | grep -q 'lib/modules/vertmp' || \
-			echo 'vendor_dlkm/lib/modules/vertmp 0 0 0644' >> ${extract_vendor_dlkm_dir}/config/vendor_dlkm_fs_config
-		cat ${extract_vendor_dlkm_dir}/config/vendor_dlkm_file_contexts | grep -q 'lib/modules/vertmp' || \
-			echo '/vendor_dlkm/lib/modules/vertmp u:object_r:vendor_file:s0' >> ${extract_vendor_dlkm_dir}/config/vendor_dlkm_file_contexts
-		ui_print "- Repacking /vendor_dlkm image..."
-		rm -f ${home}/vendor_dlkm.img
-		mkfs_erofs ${vendor_dlkm_erofs_root_dir} ${home}/vendor_dlkm.img || \
-			abort "! Failed to repack the vendor_dlkm image!"
-		rm -rf ${extract_vendor_dlkm_dir}
-	fi
-
-	unset vendor_dlkm_is_ext4 vendor_dlkm_free_space extract_vendor_dlkm_dir extract_vendor_dlkm_modules_dir \
-		vendor_dlkm_erofs_root_dir search_vendor_dlkm_modules_dir \
-		module_src module_name system_match vendor_match build_prop backup_package \
-		backup_images blocklist_expr
-#fi
-
-unset skip_update_flag do_backup_flag vendor_dlkm_block
-
-
-########## CUSTOM END ##########
-flash_generic ${vendor_dlkm_partition}
-
-# Flash kernel to boot
 flash_boot
-
-# Flash DTB to vendor_boot (only if dtb is present)
-unzip -o "$ZIPFILE" dtb -d "$home" >/dev/null 2>&1
-if [ -f "$home/dtb" ]; then
-  ui_print "- Found dtb blob, flashing to vendor_boot..."
-
-  block=${vendor_boot_partition};
-  is_slot_device=1;
-  ramdisk_compression=auto;
-  patch_vbmeta_flag=auto;
-
-  reset_ak;
-  dump_boot;
-
-  # Replace existing DTB
-  cp -f "$home/dtb" "$split_img/dtb"
-
-  write_boot;
-else
-  ui_print "! dtb blob not found, skipping vendor_boot flash"
-fi
-
-flash_dtbo
+## end boot install
--- build_kernel.sh
+++ build_kernel.sh
@@ -1,241 +1,130 @@
 #!/bin/bash
 set -e
 
-# Configuration
-DIR=$(readlink -f .)
-MAIN=$(readlink -f ${DIR}/..)
-KERNEL_DEFCONFIG=peridot_defconfig
-CLANG_DIR="$MAIN/toolchains/clang-17"
-KERNEL_DIR=$(pwd)
-OUT_DIR="$KERNEL_DIR/out"
-ZIMAGE_DIR="$OUT_DIR/arch/arm64/boot"
-DTB_DTBO_DIR="$ZIMAGE_DIR/dts/vendor/qcom"
-BUILD_START=$(date +"%s")
+DIR="$(readlink -f .)"
+MAIN="$(readlink -f "${DIR}/..")"
 
-# staging drivers
-export WLAN_CTRL_NAME=wlan
-export BOARD_PLATFORM=pineapple
-export DEVNAME=qca6750
-export MODNAME=qca_cld3_qca6750
-export DYNAMIC_SINGLE_CHIP=qca6750
-export WLAN_PROFILE=qca6750
+# Profile detection: argument first, then git branch, fallback to balanced
+ARG_PROFILE="$1"
+BRANCH="$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "auraflow-balanced")"
 
-export KERNEL_ROOT=$(pwd)
-
-# Techpack paths
-export WLAN_PLATFORM_ROOT=$(pwd)/techpack/wlan
-export AUDIO_ROOT="$KERNEL_DIR/techpack/audio-kernel"
-export CAMERA_ROOT="$KERNEL_DIR/techpack/camera-kernel"
-export DISPLAY_ROOT="$KERNEL_DIR/techpack/display-drivers"
-export GRAPHICS_ROOT="$KERNEL_DIR/techpack/graphics-kernel"
-export MM_ROOT="$KERNEL_DIR/techpack/mm-drivers"
-export MMRM_ROOT="$KERNEL_DIR/techpack/mmrm-driver"
-export SECUREMSM_ROOT="$KERNEL_DIR/techpack/securemsm-kernel"
-export SYNX_ROOT="$KERNEL_DIR/techpack/synx-kernel"
-export TOUCH_ROOT="$KERNEL_DIR/techpack/touch-drivers"
-export WLAN_ROOT="$KERNEL_DIR/techpack/wlan"
-
-# Function to check for existing Clang
-check_clang() {
-    if [ -d "$CLANG_DIR" ] && [ -f "$CLANG_DIR/bin/clang" ]; then
-        export PATH="$CLANG_DIR/bin:$PATH"
-        export KBUILD_COMPILER_STRING="$($CLANG_DIR/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-        echo "Found existing Clang: $KBUILD_COMPILER_STRING"
-        return 0
-    fi
-    return 1
-}
-
-# Install Clang if needed
-if ! check_clang; then
-    echo "No valid Clang found. Installing..."
-    echo "1. AOSP Clang (clang-r487747c)"
-    echo "2. Prelude Clang"
-    read -p "Choose [1-2]: " clang_choice
-
-    case "$clang_choice" in
-        1)
-            CLANG_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android14-release/clang-r487747c.tar.gz"
-            ARCHIVE_NAME="clang.tar.gz"
-            mkdir -p "$CLANG_DIR"
-            wget -P "$MAIN" "$CLANG_URL" -O "$MAIN/$ARCHIVE_NAME" || exit 1
-            tar -xf "$MAIN/$ARCHIVE_NAME" -C "$CLANG_DIR" --strip-components=1 || exit 1
-            rm -f "$MAIN/$ARCHIVE_NAME"
-            ;;
-        2)
-            git clone --depth=1 https://gitlab.com/jjpprrrr/prelude-clang.git "$CLANG_DIR" || exit 1
-            ;;
-        *)
-            echo "Invalid choice. Exiting..."
-            exit 1
-            ;;
-    esac
-
-    if ! check_clang; then
-        echo "Clang installation failed. Exiting..."
-        exit 1
-    fi
-fi
-
-# Set up toolchain
-export LD=ld.lld
-export ARCH=arm64
-export SUBARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-
-# Include paths
-export KERNEL_SRC="$KERNEL_DIR"
-INCLUDE_PATHS="
-    -I$KERNEL_DIR/include
-    -I$KERNEL_DIR/arch/arm64/include
-    -I$KERNEL_DIR/drivers/base/regmap
-    -I$AUDIO_ROOT/include
-    -I$CAMERA_ROOT/include
-    -I$DISPLAY_ROOT/include
-"
-
-# Build flags
-MAKE_OPTS="
-    CC=clang
-    STRIP=llvm-strip
-    LD=ld.lld
-    AR=llvm-ar
-    NM=llvm-nm
-    OBJCOPY=llvm-objcopy
-    OBJDUMP=llvm-objdump
-    HOSTCC=clang
-    HOSTCXX=clang++
-    HOSTAR=llvm-ar
-    HOSTLD=ld.lld
-    LLVM=1
-    LLVM_IAS=1
-    $INCLUDE_PATHS
-"
-
-# Apply YYLLOC workaround
-echo "Applying YYLLOC workaround..."
-YYLL1="$KERNEL_DIR/scripts/dtc/dtc-lexer.lex.c_shipped"
-YYLL2="$KERNEL_DIR/scripts/dtc/dtc-lexer.l"
-[ -f "$YYLL1" ] && sed -i "s/extern YYLTYPE yylloc/YYLTYPE yylloc/g;s/YYLTYPE yylloc/extern YYLTYPE yylloc/g" "$YYLL1"
-[ -f "$YYLL2" ] && sed -i "s/extern YYLTYPE yylloc/YYLTYPE yylloc/g;s/YYLTYPE yylloc/extern YYLTYPE yylloc/g" "$YYLL2"
-
-# Start build process
-echo "**** Building with $KBUILD_COMPILER_STRING ****"
-echo "**** Defconfig: $KERNEL_DEFCONFIG ****"
-
-# Build kernel
-make O="$OUT_DIR" $KERNEL_DEFCONFIG $MAKE_OPTS || exit 1
-make -j$(nproc --all) O="$OUT_DIR" $MAKE_OPTS || exit 1
-
-# Build modules
-BUILD_HAS_MODULES=$(grep "=m" "$OUT_DIR/.config" | wc -l)
-if [ $BUILD_HAS_MODULES -gt 0 ]; then
-    echo "Building modules..."
-    make -j$(nproc --all) O="$OUT_DIR" $MAKE_OPTS modules || exit 1
-
-    # Install modules to temporary directory
-    MODULES_DIR="$OUT_DIR/modules_temp"
-    rm -rf "$MODULES_DIR"
-    mkdir -p "$MODULES_DIR"
-    make O="$OUT_DIR" INSTALL_MOD_PATH="$MODULES_DIR" INSTALL_MOD_STRIP=1 modules_install || exit 1
-
-    # Clean up symlinks
-    find "$MODULES_DIR" -type l -delete
-fi
-
-# Restore YYLL files if in git repo
-[ -d "$KERNEL_DIR"/.git ] && git restore "$YYLL1" "$YYLL2" 2>/dev/null || true
-
-# Clean up old kernel zip files
-echo "Cleaning up old kernel zip files..."
-find "$KERNEL_DIR" -maxdepth 1 -type f -name "RealKing-Peridot-*.zip" -exec rm -v {} \;
-
-# Create temporary anykernel directory
-TIME=$(date "+%Y%m%d-%H%M%S")
-TEMP_ANY_KERNEL_DIR="$KERNEL_DIR/anykernel_temp"
-rm -rf "$TEMP_ANY_KERNEL_DIR"
-
-# Clone entire anykernel directory
-echo "Cloning anykernel directory..."
-if [ -d "$KERNEL_DIR/anykernel" ]; then
-    cp -r "$KERNEL_DIR/anykernel" "$TEMP_ANY_KERNEL_DIR"
+if [ -n "$ARG_PROFILE" ]; then
+    KERNEL_PROFILE="$ARG_PROFILE"
+elif [[ "$BRANCH" =~ "battery" ]]; then
+    KERNEL_PROFILE="Battery"
+elif [[ "$BRANCH" =~ "performance" ]]; then
+    KERNEL_PROFILE="Performance"
 else
-    echo "Error: anykernel directory not found!"
+    KERNEL_PROFILE="Balanced"
+fi
+
+KERNEL_DEFCONFIG="chenfeng_defconfig"
+
+# Locate Clang
+if [ -d "$MAIN/resources/clang" ]; then
+    CLANG_DIR="$MAIN/resources/clang"
+elif [ -d "$MAIN/clang" ]; then
+    CLANG_DIR="$MAIN/clang"
+else
+    echo "Clang compiler not found! Looked in $MAIN/resources/clang and $MAIN/clang"
     exit 1
 fi
 
-# Copy kernel image
-if [ -f "$ZIMAGE_DIR/Image.gz-dtb" ]; then
-    cp -v "$ZIMAGE_DIR/Image.gz-dtb" "$TEMP_ANY_KERNEL_DIR/"
-elif [ -f "$ZIMAGE_DIR/Image.gz" ]; then
-    cp -v "$ZIMAGE_DIR/Image.gz" "$TEMP_ANY_KERNEL_DIR/"
-elif [ -f "$ZIMAGE_DIR/Image" ]; then
-    cp -v "$ZIMAGE_DIR/Image" "$TEMP_ANY_KERNEL_DIR/"
-fi
+export PATH="$CLANG_DIR/bin:$PATH"
+export LD_LIBRARY_PATH="$CLANG_DIR/lib:$LD_LIBRARY_PATH"
+export ARCH=arm64
+export SUBARCH=arm64
 
-# Handle _modules directory
-if [ $BUILD_HAS_MODULES -gt 0 ]; then
-    echo "Preparing _modules directory..."
+export KBUILD_COMPILER_STRING="$($CLANG_DIR/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
-    # Check if _modules exists in template
-    if [ -d "$TEMP_ANY_KERNEL_DIR/_modules" ]; then
-        echo "Found existing _modules directory, clearing contents..."
-        rm -f "$TEMP_ANY_KERNEL_DIR/_modules"/*.ko
-    else
-        echo "Creating _modules directory..."
-        mkdir -p "$TEMP_ANY_KERNEL_DIR/_modules"
-    fi
+KERNEL_DIR="$(pwd)"
+ZIMAGE_DIR="$KERNEL_DIR/out/arch/arm64/boot"
+BUILD_START=$(date +"%s")
 
-    # List of specific modules to include
-    IMPORTANT_MODULES=(
-        "cnss_nl.ko" "cnss_plat_ipc_qmi_svc.ko" "cnss_prealloc.ko" "cnss_utils.ko" "cnss2.ko"
-        "goodix_core.ko" "icnss2.ko" "ipam.ko" "ipanetm.ko" "msm_drm.ko"
-        "lpass_cdc_dlkm.ko" "lpass_cdc_rx_macro_dlkm.ko" "lpass_cdc_tx_macro_dlkm.ko"
-        "lpass_cdc_va_macro_dlkm.ko" "lpass_cdc_wsa_macro_dlkm.ko" "lpass_cdc_wsa2_macro_dlkm.ko"
-        "msm_kgsl.ko" "mi_thermal_interface.ko" "qcom-cpufreq-hw.ko"
-        "qti_cpufreq_cdev.ko" "qti_devfreq_cdev.ko" "rmnet_offload.ko" "rmnet_shs.ko"
-        "smcinvoke_dlkm.ko" "wcd_core_dlkm.ko" "wcd9xxx_dlkm.ko"
-        "wcd937x_dlkm.ko" "wcd937x_slave_dlkm.ko" "wcd938x_dlkm.ko"
-        "wcd938x_slave_dlkm.ko" "wcd939x_dlkm.ko" "wcd939x_slave_dlkm.ko"
-        "wlan_firmware_service.ko" "xiaomi_touch.ko" "fs19xx_dlkm.ko" "aw882xx_dlkm.ko" "qca_cld3_qca6750.ko" "cfg80211.ko" "mac80211.ko" "qrtr-mhi.ko" "mhi.ko"
-        "spf_core_dlkm.ko" "snd_event_dlkm.ko" "gpr_dlkm.ko" "panel_event_notifier.ko" "machine_dlkm.ko" "focaltech_touch.ko" "fsa4480-i2c.ko" "ucsi_glink.ko" "msm_performance.ko"
-        "drm_display_helper.ko"
-    )
+blue='\033[0;34m'
+cyan='\033[0;36m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
 
-    # Find and copy each specified module
-    for module in "${IMPORTANT_MODULES[@]}"; do
-        find "$MODULES_DIR/lib/modules" -type f -name "$module" -exec cp -v {} "$TEMP_ANY_KERNEL_DIR/_modules/" \;
-    done
+echo -e "$cyan=================================================$nocol"
+echo -e "$cyan       BUILDING AURAFLOW KERNEL ($KERNEL_PROFILE)$nocol"
+echo -e "$cyan       Unified SM8635 / cliffs (chenfeng & peridot)    $nocol"
+echo -e "$cyan=================================================$nocol"
+echo -e "Compiler:  $KBUILD_COMPILER_STRING"
+echo -e "Clang Dir: $CLANG_DIR"
+echo -e "Defconfig: $KERNEL_DEFCONFIG"
 
-    echo "Modules in _modules:"
-    ls -lh "$TEMP_ANY_KERNEL_DIR/_modules"
-fi
+# Build defconfig
+make $KERNEL_DEFCONFIG O=out CC=clang LLVM=1 LLVM_IAS=1
 
-# Generate DTBO if not already
-echo "=========================================="
-echo "Generating DTBO from peridot-*.dtbo"
-echo "=========================================="
-scripts/mkdtboimg.py create $TEMP_ANY_KERNEL_DIR/dtbo.img \
-  $(find out/arch/arm64/boot/dts/ -name "peridot-*.dtbo" -type f)
-
-# Move Appropriate .DTB to ZIP
-echo "=========================================="
-echo "Generating DTB blob from cliffs.dtb"
-echo "=========================================="
-cat out/arch/arm64/boot/dts/vendor/qcom/cliffs.dtb > $TEMP_ANY_KERNEL_DIR/dtb
-
-# Create zip file in kernel root directory
-echo "Creating zip package..."
-ZIP_NAME="RealKing-Peridot-$TIME.zip"
-cd "$TEMP_ANY_KERNEL_DIR"
-zip -r9 "$KERNEL_DIR/$ZIP_NAME" ./*
-cd ..
-
-# Clean up temporary directory
-rm -rf "$TEMP_ANY_KERNEL_DIR"
+# Compile kernel Image
+make -j$(nproc --all) O=out \
+                      CC=clang \
+                      ARCH=arm64 \
+                      LLVM=1 \
+                      LLVM_IAS=1 \
+                      Image
 
 BUILD_END=$(date +"%s")
-DIFF=$((BUILD_END - BUILD_START))
-echo -e "\nBuild completed in $((DIFF / 60))m $((DIFF % 60))s"
-echo "Final zip: $KERNEL_DIR/$ZIP_NAME"
-echo "Zip size: $(du -h "$KERNEL_DIR/$ZIP_NAME" | cut -f1)"
+DIFF=$(($BUILD_END - $BUILD_START))
+
+if [ ! -f "$ZIMAGE_DIR/Image" ]; then
+    echo -e "$red[!] Build failed! $ZIMAGE_DIR/Image not found.$nocol"
+    exit 1
+fi
+
+TIME="$(date "+%Y%m%d-%H%M%S")"
+ZIP_NAME="Auraflow-Kernel-${KERNEL_PROFILE}-${TIME}.zip"
+BOOT_IMG_NAME="boot-auraflow-${KERNEL_PROFILE,,}.img"
+
+echo -e "$green[+] Kernel compilation completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+echo -e "$yellow[*] Packaging flashable AnyKernel3 zip...$nocol"
+
+rm -rf tmp
+mkdir -p tmp
+cp -rp ./anykernel/* tmp/
+cp -fp "$ZIMAGE_DIR/Image" tmp/Image
+cd tmp
+7za a -mx9 tmp.zip * > /dev/null
+cd ..
+cp -fp tmp/tmp.zip "$ZIP_NAME"
+rm -rf tmp
+
+echo -e "$green[+] AnyKernel3 zip created: $ZIP_NAME$nocol"
+
+# Standalone boot.img creation
+STOCK_BOOT="/mnt/android-kitchen/autoporter/project/hyperos-stock/images/boot.img"
+MAGISKBOOT="/mnt/android-kitchen/mio/bin/Linux/x86_64/magiskboot"
+
+if [ -f "$STOCK_BOOT" ] && [ -x "$MAGISKBOOT" ]; then
+    echo -e "$yellow[*] Creating standalone flashable $BOOT_IMG_NAME...$nocol"
+    REPACK_DIR="$(mktemp -d /tmp/auraflow_repack_XXXXXX)"
+    cd "$REPACK_DIR"
+    "$MAGISKBOOT" unpack -h "$STOCK_BOOT" > /dev/null 2>&1
+    cp -fp "$ZIMAGE_DIR/Image" kernel
+    "$MAGISKBOOT" repack "$STOCK_BOOT" "$KERNEL_DIR/$BOOT_IMG_NAME" > /dev/null 2>&1
+    cd "$KERNEL_DIR"
+    rm -rf "$REPACK_DIR"
+
+    if [ -f "$KERNEL_DIR/$BOOT_IMG_NAME" ]; then
+        echo -e "$green[+] Standalone boot image created: $BOOT_IMG_NAME$nocol"
+        echo -e "    Header details:"
+        (
+            TMP_CHECK="$(mktemp -d /tmp/auraflow_check_XXXXXX)"
+            cd "$TMP_CHECK"
+            "$MAGISKBOOT" unpack -h "$KERNEL_DIR/$BOOT_IMG_NAME" 2>&1 | sed 's/^/    /'
+            cd "$KERNEL_DIR"
+            rm -rf "$TMP_CHECK"
+        )
+    fi
+else
+    echo -e "$yellow[!] Stock boot.img or magiskboot not found, skipping standalone boot.img creation.$nocol"
+fi
+
+echo -e "$green=================================================$nocol"
+echo -e "$green   Build complete! Output files:$nocol"
+echo -e "   - AnyKernel3 Zip:  $KERNEL_DIR/$ZIP_NAME"
+[ -f "$KERNEL_DIR/$BOOT_IMG_NAME" ] && echo -e "   - Standalone Boot: $KERNEL_DIR/$BOOT_IMG_NAME"
+echo -e "$green=================================================$nocol"
